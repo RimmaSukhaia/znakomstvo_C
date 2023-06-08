30014d703514c10ea4dca61f4669b73c87fc34c7
Rimma <s21rimma@mail.ru>
2023-06-08 23:31:19 +0300

    dobavila zadaniya

--- lesson/les_1/Program.cs
+++ lesson/les_1/Program.cs
@@ -1,1 +1,42 @@
 ﻿
+/*
+//  программа для сложение 2х чисел
+System.Console.WriteLine("введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+System.Console.Write("сумма ");
+System.Console.WriteLine(num1+num2);
+
+
+//программа на деление с остатком
+
+double num1 = 12;
+double num2 = 5;
+System.Console.WriteLine(num1/num2);
+
+
+// программа с случайными числами
+
+int num1 = new Random().Next(0, 10);
+System.Console.WriteLine(num1);
+int num2 = new Random().Next(0, 10);
+System.Console.WriteLine(num2);
+int result = num1 + num2;
+System.Console.WriteLine(result);
+
+
+//программа на приветствие
+
+System.Console.WriteLine("Введите имя пользователя:  ");
+string username = Console.ReadLine();
+if(username.ToLower() == "римма")
+{
+    System.Console.WriteLine("Ура! Это же Римма!");
+}
+else
+{
+    System.Console.WriteLine("Привет, "+username);
+}
+
+*/
